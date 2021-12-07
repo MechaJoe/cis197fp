8c1fdcfc9c4bddcb132036aced32ad9136a0aa36
Joe Konno <53798807+MechaJoe@users.noreply.github.com>
2021-12-06 22:11:22 -0500

    Update _config.js

--- backend/_config.js
+++ backend/_config.js
@@ -1,12 +1,12 @@
 const ids = {
   google: {
-    clientID: '893129803163-gkh7t8mt7vqpmjq93vq77o5unaop19s2.apps.googleusercontent.com',
-    clientSecret: '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',
+    clientID: 'get_your_own',
+    clientSecret: 'get_your_own',
     callbackURL: 'http://127.0.0.1:3000/auth/google/callback',
   },
   linkedin: {
-    clientID: '778iog1d81wmcx',
-    clientSecret: 'qRWvF2AWcjm9nOYw',
+    clientID: 'get_your_own',
+    clientSecret: 'get_your_own',
     callbackURL: 'http://127.0.0.1:3000/auth/linkedin/callback',
   },
   twitter: {
